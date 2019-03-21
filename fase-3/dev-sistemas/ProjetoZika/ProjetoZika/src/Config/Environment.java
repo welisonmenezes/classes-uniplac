b3a2e51e581c21287d2cb5178949ad947895b112
welisonmenezes <welisonmenezes@hotmail.com>
2019-03-21 13:07:34 -0300

    Add translation to Config files

--- fase-3/dev-sistemas/ProjetoZika/ProjetoZika/src/Config/Environment.java
+++ fase-3/dev-sistemas/ProjetoZika/ProjetoZika/src/Config/Environment.java
@@ -9,6 +9,7 @@ import Models.Permissao;
 import Models.Setor;
 import Models.Status;
 import Models.Unidade;
+import Utils.Methods;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,38 +22,37 @@ public class Environment {
     public static final ArrayList<Unidade> UNIDADES = new ArrayList<Unidade>();
     static {
         UNIDADES.add(new Unidade(0, ""));
-        UNIDADES.add(new Unidade(1, "Quilo"));
-        UNIDADES.add(new Unidade(2, "Litro"));
-        UNIDADES.add(new Unidade(3, "Caixa"));
-        UNIDADES.add(new Unidade(4, "Unidade"));
-        
+        UNIDADES.add(new Unidade(1, Methods.getTranslation("Quilo")));
+        UNIDADES.add(new Unidade(2, Methods.getTranslation("Litro")));
+        UNIDADES.add(new Unidade(3, Methods.getTranslation("Caixa")));
+        UNIDADES.add(new Unidade(4, Methods.getTranslation("Pacote")));
+        UNIDADES.add(new Unidade(5, Methods.getTranslation("Unidade")));
     };
     
     public static final ArrayList<Permissao> PERMISSOES = new ArrayList<Permissao>();
     static {
         PERMISSOES.add(new Permissao(0, ""));
-        PERMISSOES.add(new Permissao(1, "Usuário"));
-        PERMISSOES.add(new Permissao(2, "Almoxerife"));
-        PERMISSOES.add(new Permissao(3, "Administrador"));
+        PERMISSOES.add(new Permissao(1, Methods.getTranslation("Usuario")));
+        PERMISSOES.add(new Permissao(2, Methods.getTranslation("Almoxarife")));
+        PERMISSOES.add(new Permissao(3, Methods.getTranslation("Administrador")));
     };
     
     public static final ArrayList<Setor> SETORES = new ArrayList<Setor>();
     static {
         SETORES.add(new Setor(0, ""));
-        SETORES.add(new Setor(1, "Recursos Humanos"));
-        SETORES.add(new Setor(2, "Contabilidade"));
-        SETORES.add(new Setor(3, "Manutenção"));
-        SETORES.add(new Setor(4, "Administração"));
-        SETORES.add(new Setor(5, "Almixerifado"));
+        SETORES.add(new Setor(1, Methods.getTranslation("RecursosHumanos")));
+        SETORES.add(new Setor(2, Methods.getTranslation("Contabilidade")));
+        SETORES.add(new Setor(3, Methods.getTranslation("Manutencao")));
+        SETORES.add(new Setor(4, Methods.getTranslation("Administracao")));
+        SETORES.add(new Setor(5, Methods.getTranslation("Almoxarifado")));
     };
     
     public static final ArrayList<Status> STATUS = new ArrayList<Status>();
     static {
         STATUS.add(new Status(0, ""));
-        STATUS.add(new Status(0, "Pendente"));
-        STATUS.add(new Status(0, "Aguardando Entrega"));
-        STATUS.add(new Status(0, "Aprovado"));
-        STATUS.add(new Status(0, "Aprovado Parcial"));
-        STATUS.add(new Status(0, "Negado"));
+        STATUS.add(new Status(1, Methods.getTranslation("Pendente")));
+        STATUS.add(new Status(2, Methods.getTranslation("AguardandoEntrega")));
+        STATUS.add(new Status(3, Methods.getTranslation("Finalizado")));
+        STATUS.add(new Status(4, Methods.getTranslation("Negado")));
     };
 }
